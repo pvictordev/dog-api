44feaa6dcb084209e2d005c18b78b3e2b44cc5c6
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-29 18:43:41 +0300

    setted up routing and the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BreedComponent } from './pages/breed/breed.component';
+import { DogsComponent } from './pages/dogs/dogs.component';
+import { SubBreedComponent } from './pages/sub-breed/sub-breed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DogsComponent },
+  { path: 'breed', component: BreedComponent },
+  { path: 'sub-breed', component: SubBreedComponent },
+];
---
+++ src/app/pages/breed/breed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breed',
+  standalone: true,
+  imports: [],
+  templateUrl: './breed.component.html',
+  styleUrl: './breed.component.css'
+})
+export class BreedComponent {
+
+}
