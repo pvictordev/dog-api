5d64aafa6d9bc1e13ec3b5884dc61deece6a864c
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-30 10:30:20 +0300

    added component to handle unknown routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { BreedComponent } from './pages/breed/breed.component';
 import { DogsComponent } from './pages/dogs/dogs.component';
 import { SubBreedComponent } from './pages/sub-breed/sub-breed.component';
+import { PageNotFoundComponent } from './erros/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: DogsComponent },
   { path: 'breed/:breedName', component: BreedComponent },
   { path: 'breed/:breedName/:subBreedName', component: SubBreedComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
