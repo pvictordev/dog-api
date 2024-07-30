36a7bede68b49d68e9ab67807dcae051932a01e5
Victor Purice <victor.purice@ulbsibiu.ro>
2024-07-30 15:05:34 +0300

    added search feature for breeds

--- src/app/pages/dogs/dogs.component.ts
+++ src/app/pages/dogs/dogs.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dogs',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './dogs.component.html',
   styleUrl: './dogs.component.css',
   providers: [ApiService]
@@ -15,13 +16,17 @@ export class DogsComponent implements OnInit {
   constructor(private apiService: ApiService) { }
 
   breeds: string[] = [];
+  searchItem: string = '';
+  filteredBreeds: string[] = [];
 
   ngOnInit(): void {
     this.fetchAllBreeds();
+    // this.fetchPartialBreeds();
+    this.filteredBreeds = this.breeds
   }
 
   fetchAllBreeds(): void {
-    this.apiService.getAllBreeds().subscribe({
+    this.apiService.getBreeds().subscribe({
       next: (data) => {
         this.breeds = Object.keys(data.message);
       },
@@ -29,6 +34,25 @@ export class DogsComponent implements OnInit {
         console.error("Error fetching dogs:", error);
       },
     });
+  }
+
+  // fetchPartialBreeds(): void {
+  //   this.apiService.getPartialBreeds(10).subscribe({
+  //     next: (data) => {
+  //       console.log(data)
+  //       this.breeds = data;
+  //     },
+  //     error: (error) => {
+  //       console.error("Error fetching dogs:", error);
+  //     },
+  //   });
+  // }
+
+  onSearch(): void {
+    const searchItemLower = this.searchItem.toLowerCase().trim();
+    this.filteredBreeds = this.breeds.filter(breed =>
+      breed.toLowerCase().startsWith(searchItemLower)
+    );
   }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +9,18 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAllBreeds(): Observable<any> {
+  getBreeds(): Observable<any> {
     return this.httpClient.get<any>('https://dog.ceo/api/breeds/list/all');
   }
+
+  // getPartialBreeds(limit: number = 10): Observable<string[]> {
+  //   return this.httpClient.get<any>('https://dog.ceo/api/breeds/list/all').pipe(
+  //     map(response => {
+  //       const allBreeds = Object.keys(response.message);
+  //       return allBreeds.slice(0, limit);
+  //     })
+  //   );
+  // }
 
   getBreedImage(breedName: string | null): Observable<any> {
     return this.httpClient.get<any>("https://dog.ceo/api/breed/" + breedName + "/images");
